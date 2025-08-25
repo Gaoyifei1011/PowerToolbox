65f60495a64febca07f9e8e25006b1cc777682b6
怡飞 高 <363301617@qq.com>
2025-08-26 00:33:42 +0800

    Remove User32 API
    Remove User32 API

--- PowerToolbox/WindowsAPI/PInvoke/User32/User32Library.cs
+++ PowerToolbox/WindowsAPI/PInvoke/User32/User32Library.cs
@@ -126,15 +126,6 @@ namespace PowerToolbox.WindowsAPI.PInvoke.User32
         public static extern bool EndPaint(IntPtr hWnd, [In] ref PAINTSTRUCT lpPaint);
 
         /// <summary>
-        /// 指向应用程序定义的回调函数的指针。 有关详细信息，请参阅 EnumWindowsProc。
-        /// </summary>
-        /// <param name="lpEnumFunc">指向应用程序定义的回调函数的指针。 有关详细信息，请参阅 EnumWindowsProc。</param>
-        /// <param name="lParam">要传递给回调函数的应用程序定义值。</param>
-        /// <returns>如果该函数成功，则返回值为非零值。如果函数失败，则返回值为零。 </returns>
-        [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "EnumWindows", PreserveSig = true, SetLastError = false)]
-        public static extern bool EnumWindows(WNDENUMPROC lpEnumFunc, IntPtr lParam);
-
-        /// <summary>
         /// FillRect 函数使用指定的画笔填充矩形。 此函数包括左边框和上边框，但不包括矩形的右边框和下边框。
         /// </summary>
         /// <param name="hdc">设备上下文的句柄。</param>
@@ -206,15 +197,6 @@ namespace PowerToolbox.WindowsAPI.PInvoke.User32
         /// <returns>如果函数成功，则返回值是请求的值。如果函数失败，则返回值为零。</returns>
         [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "GetWindowLongPtrW", PreserveSig = true, SetLastError = false)]
         public static extern int GetWindowLongPtr(IntPtr hWnd, WindowLongIndexFlags nIndex);
-
-        /// <summary>
-        /// 检索创建指定窗口的线程的标识符，以及创建该窗口的进程（可选）的标识符。
-        /// </summary>
-        /// <param name="hWnd">窗口的句柄。</param>
-        /// <param name="processId">指向接收进程标识符的变量的指针。 如果此参数不为 NULL， 则 GetWindowThreadProcessId 会将进程的标识符复制到变量;否则，它不会。 如果函数失败，则变量的值保持不变。</param>
-        /// <returns>如果函数成功，则返回值是创建窗口的线程的标识符。 如果窗口句柄无效，则返回值为零。</returns>
-        [DllImport(User32, CharSet = CharSet.Unicode, EntryPoint = "GetWindowThreadProcessId", PreserveSig = true, SetLastError = false)]
-        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
         /// <summary>
         /// 确定指定窗口的可见性状态。
