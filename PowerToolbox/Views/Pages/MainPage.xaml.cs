01ebfee7a65cacee1a00cde676956eb4f616ad53
怡飞 高 <363301617@qq.com>
2025-09-12 01:42:37 +0800

    Fix error running elevated prompt in UpdateManagerPage(https://github.com/Gaoyifei1011/PowerToolbox/issues/3)
    Fix error running elevated prompt in UpdateManagerPage(https://github.com/Gaoyifei1011/PowerToolbox/issues/3)

--- PowerToolbox/Views/Pages/MainPage.xaml.cs
+++ PowerToolbox/Views/Pages/MainPage.xaml.cs
@@ -380,7 +380,7 @@ namespace PowerToolbox.Views.Pages
                 }
 
                 // 如果导航到更新页面、 Hosts 文件编辑器页面、文件恢复页面，而且是非管理员模式，显示提示对话框
-                if ((Equals(currentPageType, typeof(UpdateManagerPage)) || Equals(currentPageType, typeof(HostsPage))) || Equals(currentPageType, typeof(WinFRPage)) && !RuntimeHelper.IsElevated)
+                if ((Equals(currentPageType, typeof(UpdateManagerPage)) || Equals(currentPageType, typeof(HostsPage)) || Equals(currentPageType, typeof(WinFRPage))) && !RuntimeHelper.IsElevated)
                 {
                     await MainWindow.Current.ShowDialogAsync(new NeedElevatedDialog());
                     NavigationFrom();
