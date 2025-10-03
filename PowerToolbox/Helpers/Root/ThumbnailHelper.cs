93fa83207b11bbf4cb01f2a517f36cf5fb907771
怡飞 高 <363301617@qq.com>
2025-10-03 12:40:43 +0800

    Add ThumbnailHelper for get program thumbnail
    Add ThumbnailHelper for get program thumbnail

---
+++ PowerToolbox/Helpers/Root/ThumbnailHelper.cs
@@ -0,0 +1,97 @@
+﻿using PowerToolbox.Services.Root;
+using PowerToolbox.WindowsAPI.ComTypes;
+using PowerToolbox.WindowsAPI.PInvoke.Shell32;
+using System;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace PowerToolbox.Helpers.Root
+{
+    /// <summary>
+    /// 文件缩略图辅助类
+    /// </summary>
+    public static class ThumbnailHelper
+    {
+        /// <summary>
+        /// 获取文件缩略图
+        /// </summary>
+        public static Bitmap GetThumbnailBitmap(string filePath)
+        {
+            try
+            {
+                int result = Shell32Library.SHCreateItemFromParsingName(filePath, null, typeof(IShellItem).GUID, out IShellItem shellItem);
+
+                if (result is 0)
+                {
+                    result = ((IShellItemImageFactory)shellItem).GetImage(new Size(256, 256), SIIGBF.SIIGBF_RESIZETOFIT, out IntPtr hBitmap);
+                    Marshal.ReleaseComObject(shellItem);
+
+                    if (result is 0)
+                    {
+                        Bitmap bitmap = System.Drawing.Image.FromHbitmap(hBitmap);
+
+                        if (System.Drawing.Image.GetPixelFormatSize(bitmap.PixelFormat) < 32)
+                        {
+                            return bitmap;
+                        }
+                        else
+                        {
+                            return CreateAlphaBitmap(bitmap, PixelFormat.Format32bppArgb);
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                LogService.WriteLog(EventLevel.Error, nameof(PowerToolbox), nameof(ThumbnailHelper), nameof(GetThumbnailBitmap), 1, e);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 创建 Alpha 的 Bitmap
+        /// </summary>
+        private static Bitmap CreateAlphaBitmap(Bitmap srcBitmap, PixelFormat targetPixelFormat)
+        {
+            Bitmap bitmap = new(srcBitmap.Width, srcBitmap.Height, targetPixelFormat);
+            Rectangle bitmapBound = new(0, 0, srcBitmap.Width, srcBitmap.Height);
+            BitmapData srcData = srcBitmap.LockBits(bitmapBound, ImageLockMode.ReadOnly, srcBitmap.PixelFormat);
+            bool isAlplaBitmap = false;
+
+            try
+            {
+                for (int y = 0; y <= srcData.Height - 1; y++)
+                {
+                    for (int x = 0; x <= srcData.Width - 1; x++)
+                    {
+                        Color pixelColor = Color.FromArgb(
+                            Marshal.ReadInt32(srcData.Scan0, (srcData.Stride * y) + (4 * x)));
+
+                        if (pixelColor.A > 0 & pixelColor.A < 255)
+                        {
+                            isAlplaBitmap = true;
+                        }
+
+                        bitmap.SetPixel(x, y, pixelColor);
+                    }
+                }
+            }
+            finally
+            {
+                srcBitmap.UnlockBits(srcData);
+            }
+
+            return isAlplaBitmap ? bitmap : srcBitmap;
+        }
+    }
+}
--- PowerToolbox/Views/Pages/DownloadManagerPage.xaml.cs
+++ PowerToolbox/Views/Pages/DownloadManagerPage.xaml.cs
@@ -778,7 +778,7 @@ namespace PowerToolbox.Views.Pages
                 MemoryStream memoryStream = null;
                 try
                 {
-                    Bitmap thumbnailBitmap = GetThumbnailBitmap(downloadScheduler.FilePath);
+                    Bitmap thumbnailBitmap = ThumbnailHelper.GetThumbnailBitmap(downloadScheduler.FilePath);
 
                     if (thumbnailBitmap is not null)
                     {
@@ -884,85 +884,5 @@ namespace PowerToolbox.Views.Pages
         }
 
         #endregion 第三部分：下载管理页面——自定义事件
-
-        /// <summary>
-        /// 获取文件缩略图
-        /// </summary>
-        private Bitmap GetThumbnailBitmap(string filePath)
-        {
-            try
-            {
-                int result = Shell32Library.SHCreateItemFromParsingName(filePath, null, typeof(IShellItem).GUID, out IShellItem shellItem);
-
-                if (result is 0)
-                {
-                    result = ((IShellItemImageFactory)shellItem).GetImage(new Size(256, 256), SIIGBF.SIIGBF_RESIZETOFIT, out IntPtr hBitmap);
-                    Marshal.ReleaseComObject(shellItem);
-
-                    if (result is 0)
-                    {
-                        Bitmap bitmap = System.Drawing.Image.FromHbitmap(hBitmap);
-
-                        if (System.Drawing.Image.GetPixelFormatSize(bitmap.PixelFormat) < 32)
-                        {
-                            return bitmap;
-                        }
-                        else
-                        {
-                            return CreateAlphaBitmap(bitmap, PixelFormat.Format32bppArgb);
-                        }
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception e)
-            {
-                LogService.WriteLog(EventLevel.Error, nameof(PowerToolbox), nameof(DownloadManagerPage), nameof(GetThumbnailBitmap), 1, e);
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// 创建 Alpha 的 Bitmap
-        /// </summary>
-        private static Bitmap CreateAlphaBitmap(Bitmap srcBitmap, PixelFormat targetPixelFormat)
-        {
-            Bitmap bitmap = new(srcBitmap.Width, srcBitmap.Height, targetPixelFormat);
-            Rectangle bitmapBound = new(0, 0, srcBitmap.Width, srcBitmap.Height);
-            BitmapData srcData = srcBitmap.LockBits(bitmapBound, ImageLockMode.ReadOnly, srcBitmap.PixelFormat);
-            bool isAlplaBitmap = false;
-
-            try
-            {
-                for (int y = 0; y <= srcData.Height - 1; y++)
-                {
-                    for (int x = 0; x <= srcData.Width - 1; x++)
-                    {
-                        Color pixelColor = Color.FromArgb(
-                            Marshal.ReadInt32(srcData.Scan0, (srcData.Stride * y) + (4 * x)));
-
-                        if (pixelColor.A > 0 & pixelColor.A < 255)
-                        {
-                            isAlplaBitmap = true;
-                        }
-
-                        bitmap.SetPixel(x, y, pixelColor);
-                    }
-                }
-            }
-            finally
-            {
-                srcBitmap.UnlockBits(srcData);
-            }
-
-            return isAlplaBitmap ? bitmap : srcBitmap;
-        }
     }
 }
