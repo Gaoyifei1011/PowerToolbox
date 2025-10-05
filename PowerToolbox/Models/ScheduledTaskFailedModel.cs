925b101d738bc2c225649351d3aac1c91e1321ab
怡飞 高 <363301617@qq.com>
2025-10-05 10:49:34 +0800

    Add ScheduledTaskFailedModel
    Add ScheduledTaskFailedModel

---
+++ PowerToolbox/Models/ScheduledTaskFailedModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PowerToolbox.Models
+{
+    /// <summary>
+    /// 失败计划任务数据模型
+    /// </summary>
+    public class ScheduledTaskFailedModel
+    {
+        /// <summary>
+        /// 计划任务名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 计划任务路径
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        public Exception Exception { get; set; }
+    }
+}
