2df69e24f5494dea5b4d2dec13c523d2447ffafe
怡飞 高 <363301617@qq.com>
2025-10-06 10:43:33 +0800

    Add ExperimentalFeatureManagerPage
    Add ExperimentalFeatureManagerPage

---
+++ PowerToolbox/Views/Pages/ExperimentalFeatureManagerPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
+
+namespace PowerToolbox.Views.Pages
+{
+    /// <summary>
+    /// 实验功能管理界面
+    /// </summary>
+    public sealed partial class ExperimentalFeatureManagerPage : Page, INotifyPropertyChanged
+    {
+        private string _experimentaFeatureDescription;
+
+        public string ExperimentalFeatureDescription
+        {
+            get { return _experimentaFeatureDescription; }
+
+            set
+            {
+                if (!string.Equals(_experimentaFeatureDescription, value))
+                {
+                    _experimentaFeatureDescription = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ExperimentalFeatureDescription)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ExperimentalFeatureManagerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
