9b04b50a765f1436ceab5a846e10601aa3226d3e
怡飞 高 <363301617@qq.com>
2025-10-14 00:46:23 +0800

    Update data encrypt type
    Update data encrypt type

---
+++ PowerToolbox/Extensions/DataType/Enums/DataEncryptType.cs
@@ -0,0 +1,26 @@
+﻿namespace PowerToolbox.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 数据加密类型
+    /// </summary>
+    public enum DataEncryptType
+    {
+        AES = 0,
+        Blowfish = 1,
+        CaesarCipher = 2,
+        ChaCha20 = 3,
+        DES = 4,
+        ECC = 5,
+        MorseCode = 6,
+        Rabbit = 7,
+        RC2 = 8,
+        RC4 = 9,
+        RC5 = 10,
+        RC6 = 11,
+        RSA = 12,
+        SM2 = 13,
+        SM4 = 14,
+        TripleDES = 15,
+        XOR = 16,
+    }
+}
