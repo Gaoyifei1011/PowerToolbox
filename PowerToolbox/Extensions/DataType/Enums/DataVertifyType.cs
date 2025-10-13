afc37e9c1ec984abb9863014c5e882d28cb2125d
怡飞 高 <363301617@qq.com>
2025-10-14 00:46:09 +0800

    Update data vertify type
    Update data vertify type

---
+++ PowerToolbox/Extensions/DataType/Enums/DataVertifyType.cs
@@ -0,0 +1,49 @@
+﻿namespace PowerToolbox.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 数据校验类型
+    /// </summary>
+    public enum DataVertifyType
+    {
+        AICH = 0,
+        BLAKE2sp = 1,
+        BLAKE3 = 2,
+        BTIH = 3,
+        CRC_32 = 4,
+        CRC_64 = 5,
+        ED2K = 6,
+        EDON_R_224 = 7,
+        EDON_R_256 = 8,
+        EDON_R_384 = 9,
+        EDON_R_512 = 10,
+        GOST94 = 11,
+        GOST94CryptoPro = 12,
+        GOST12_256 = 13,
+        GOST12_512 = 14,
+        HAS_160 = 15,
+        MD2 = 16,
+        MD4 = 17,
+        MD5 = 18,
+        RIPEMD_160 = 19,
+        SHA_1 = 20,
+        SHA_224 = 21,
+        SHA_256 = 22,
+        SHA_384 = 23,
+        SHA_512 = 24,
+        SHA3_224 = 25,
+        SHA3_256 = 26,
+        SHA3_384 = 27,
+        SHA3_512 = 28,
+        SM3 = 29,
+        SNEFRU_128 = 30,
+        SNEFRU_256 = 31,
+        TIGER = 32,
+        TIGER2 = 33,
+        TTH = 34,
+        WHIRLPOOL = 35,
+        XXH32 = 36,
+        XXH64 = 37,
+        XXH3 = 38,
+        XXH128 = 39,
+    }
+}
