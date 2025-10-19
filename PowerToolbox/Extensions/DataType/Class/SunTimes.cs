9f009006ec0982d0dc9268b3bac179d99086ca0c
怡飞 高 <363301617@qq.com>
2025-10-19 09:53:53 +0800

    Add sunrise and sunset calculate implementation
    Add sunrise and sunset calculate implementation

---
+++ PowerToolbox/Extensions/DataType/Class/SunTimes.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerToolbox.Extensions.DataType.Class
+{
+    public class SunTimes
+    {
+        public int SunriseHour;
+        public int SunriseMinute;
+        public int SunsetHour;
+        public int SunsetMinute;
+        public string Text;
+        public bool HasSunrise;
+        public bool HasSunset;
+    }
+}
