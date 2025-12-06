bc66f49830169be0bab0cdd6c8a114f8f06d9533
怡飞 高 <363301617@qq.com>
2025-12-07 01:34:45 +0800

    Add MorseCode Algorithm encrypt implementation
    Add MorseCode Algorithm encrypt implementation

---
+++ PowerToolbox/Extensions/Encrypt/MorseCode.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace PowerToolbox.Extensions.Encrypt
+{
+    /// <summary>
+    /// 摩尔斯密码加密算法实现
+    /// </summary>
+    public static class MorseCode
+    {
+        private static Dictionary<char, string> charToMorse = new Dictionary<char, string>()
+        {
+            {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+            {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+            {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+            {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+            {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+            {'Z', "--.."}, {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"},
+            {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."}, {'8', "---.."},
+            {'9', "----."}
+        };
+
+        private static readonly Dictionary<string, char> morseToChar = [];
+
+        static MorseCode()
+        {
+            foreach (KeyValuePair<char, string> pair in charToMorse)
+            {
+                morseToChar[pair.Value] = pair.Key;
+            }
+        }
+
+        /// <summary>
+        /// 摩尔斯密码加密
+        /// </summary>
+        public static string Encode(string encodeText)
+        {
+            string encoded = string.Empty;
+            foreach (char c in encodeText)
+            {
+                if (charToMorse.TryGetValue(c, out string value))
+                {
+                    encoded += value + " ";
+                }
+                else
+                {
+                    encoded += " ";
+                }
+            }
+            return encoded.Trim();
+        }
+
+        /// <summary>
+        /// 摩尔斯密码解密
+        /// </summary>
+        public static string Decode(string morseCode)
+        {
+            string[] words = morseCode.Split(' ');
+            string decoded = string.Empty;
+            foreach (string word in words)
+            {
+                string[] letters = word.Split(' ');
+                foreach (string letter in letters)
+                {
+                    if (morseToChar.TryGetValue(letter, out char value))
+                    {
+                        decoded += value;
+                    }
+                }
+            }
+            return decoded.Trim();
+        }
+    }
+}
--- PowerToolbox/Views/Pages/DataEncryptPage.xaml.cs
+++ PowerToolbox/Views/Pages/DataEncryptPage.xaml.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -1138,6 +1137,22 @@ namespace PowerToolbox.Views.Pages
                     }
                 case DataEncryptType.MorseCode:
                     {
+                        if (selectedEncryptIndex is 0)
+                        {
+                            //TOOD：显示通知：摩尔斯密码仅支持字符串加密
+                        }
+                        else
+                        {
+                            try
+                            {
+                                // 隐藏选项
+                                encryptedData = MorseCode.Encode(contentData.ToUpperInvariant());
+                            }
+                            catch (Exception e)
+                            {
+                                LogService.WriteLog(TraceEventType.Error, nameof(PowerToolbox), nameof(DataEncryptPage), nameof(GetEncryptedData), Convert.ToInt32(DataEncryptType.CaesarCipher) + 1, e);
+                            }
+                        }
                         break;
                     }
                 case DataEncryptType.Rabbit:
