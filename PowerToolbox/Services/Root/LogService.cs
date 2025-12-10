b15d764d408061baf66563c424074ecdc2c0d1d3
怡飞 高 <363301617@qq.com>
2025-12-11 01:27:47 +0800

    Fix LogService unknown exception
    Fix LogService unknown exception

--- PowerToolbox/Services/Root/LogService.cs
+++ PowerToolbox/Services/Root/LogService.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using Microsoft.VisualBasic.Logging;
-using PowerToolbox.WindowsAPI.PInvoke.Shell32;
+﻿using PowerToolbox.WindowsAPI.PInvoke.Shell32;
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 
 // 抑制 CA1806 警告
 #pragma warning disable CA1806
@@ -21,7 +19,6 @@ namespace PowerToolbox.Services.Root
         private static readonly string notAvailable = "N/A";
         private static SemaphoreSlim logSemaphoreSlim = new(1, 1);
         private static bool isInitialized = false;
-        private static FileLogTraceListener fileLogTraceListener = new();
         private static DirectoryInfo logDirectory;
 
         /// <summary>
@@ -47,16 +44,6 @@ namespace PowerToolbox.Services.Root
                     return;
                 }
             }
-
-            fileLogTraceListener.Append = false;
-            fileLogTraceListener.AutoFlush = true;
-            fileLogTraceListener.Encoding = Encoding.UTF8;
-
-            if (logDirectory is not null)
-            {
-                fileLogTraceListener.Location = LogFileLocation.Custom;
-                fileLogTraceListener.CustomLocation = logDirectory.FullName;
-            }
         }
 
         /// <summary>
@@ -70,69 +57,90 @@ namespace PowerToolbox.Services.Root
                 {
                     logSemaphoreSlim?.Wait();
 
-                    if (fileLogTraceListener is not null)
+                    try
                     {
-                        try
+                        if (!Directory.Exists(logDirectory.FullName))
                         {
-                            if (logDirectory is not null && !Directory.Exists(logDirectory.FullName))
-                            {
-                                Directory.CreateDirectory(logDirectory.FullName);
-                            }
-
-                            string logFileName = string.Format("Logs-{0}-{1}-{2}-{3:D2}-{4}", nameSpaceName, className, methodName, index, DateTimeOffset.Now.ToString("yyyy-MM-dd HH-mm-ss.fff"));
-
-                            StringBuilder stringBuilder = new();
-                            stringBuilder.AppendLine("Session");
-                            stringBuilder.AppendLine("Exception log session");
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Channel");
-                            stringBuilder.AppendLine("Exception log channel");
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("ActivityId");
-                            stringBuilder.AppendLine(Guid.NewGuid().ToString("B"));
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Level");
-                            stringBuilder.AppendLine(Convert.ToString(traceEventType));
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("NameSpace");
-                            stringBuilder.AppendLine(nameSpaceName);
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Class");
-                            stringBuilder.AppendLine(className);
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Method");
-                            stringBuilder.AppendLine(methodName);
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Index");
-                            stringBuilder.AppendLine(Convert.ToString(index));
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("HelpLink");
-                            stringBuilder.AppendLine(string.IsNullOrEmpty(exception.HelpLink) ? notAvailable : exception.HelpLink.Replace('\r', ' ').Replace('\n', ' '));
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Message");
-                            stringBuilder.AppendLine(string.IsNullOrEmpty(exception.Message) ? notAvailable : exception.Message.Replace('\r', ' ').Replace('\n', ' '));
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("HResult");
-                            stringBuilder.AppendLine(Convert.ToString(exception.HResult, 16).ToUpperInvariant());
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("Source");
-                            stringBuilder.AppendLine(string.IsNullOrEmpty(exception.Source) ? notAvailable : exception.Source.Replace('\r', ' ').Replace('\n', ' '));
-                            stringBuilder.AppendLine("=========================================================");
-                            stringBuilder.AppendLine("StackTrace");
-                            stringBuilder.AppendLine(string.IsNullOrEmpty(exception.StackTrace) ? notAvailable : exception.StackTrace.Replace('\r', ' ').Replace('\n', ' '));
-                            stringBuilder.AppendLine("=========================================================");
-
-                            fileLogTraceListener.BaseFileName = logFileName;
-                            fileLogTraceListener.WriteLine(stringBuilder.ToString());
+                            Directory.CreateDirectory(logDirectory.FullName);
                         }
-                        catch (Exception)
-                        {
-                            return;
-                        }
-                        finally
-                        {
-                            logSemaphoreSlim?.Release();
-                        }
+
+                        string logFileName = string.Format("Logs-{0}-{1}-{2}-{3:D2}-{4}.xml", nameSpaceName, className, methodName, index, DateTimeOffset.Now.ToString("yyyy-MM-dd HH-mm-ss.fff"));
+
+                        /*
+                        <LogRecord>
+                         <Session>Exception log session</Session>
+                         <Channel>Exception log channel</Channel>
+                         <ActivityId>Guid.NewGuid().ToString("B")</ActivityId>
+                         <Data>
+                          <Level>Convert.ToString(eventLevel)</Level>
+                          <NameSpace>nameSpaceName</NameSpace>
+                          <Class>className</Class>
+                          <Method>methodName</Method>
+                          <Index>index</Index>
+                          <HelpLink>string.IsNullOrEmpty(exception.HelpLink) ? unknown : exception.HelpLink.Replace('\r', ' ').Replace('\n', ' ')</HelpLink>
+                          <Message>string.IsNullOrEmpty(exception.Message) ? unknown : exception.Message.Replace('\r', ' ').Replace('\n', ' ')</Message>
+                          <HResult>Convert.ToString(exception.HResult, 16).ToUpper()</HResult>
+                          <Source>string.IsNullOrEmpty(exception.Source) ? unknown : exception.Source.Replace('\r', ' ').Replace('\n', ' ')</Source>
+                          <StackTrace>string.IsNullOrEmpty(exception.StackTrace) ? unknown : exception.StackTrace.Replace('\r', ' ').Replace('\n', ' ')</StackTrace>
+                         </Data>
+                        </LogRecord>
+                        */
+                        XmlDocument xmlDocument = new();
+                        XmlDeclaration xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null);
+                        xmlDocument.AppendChild(xmlDeclaration);
+                        XmlElement logRecordElement = xmlDocument.CreateElement("LogRecord");
+                        xmlDocument.AppendChild(logRecordElement);
+                        XmlElement sessionElement = xmlDocument.CreateElement("Session");
+                        sessionElement.InnerText = "Exception log session";
+                        logRecordElement.AppendChild(sessionElement);
+                        XmlElement channelElement = xmlDocument.CreateElement("Channel");
+                        channelElement.InnerText = "Exception log channel";
+                        logRecordElement.AppendChild(channelElement);
+                        XmlElement activityIdElement = xmlDocument.CreateElement("ActivityId");
+                        activityIdElement.InnerText = Guid.NewGuid().ToString("B");
+                        logRecordElement.AppendChild(activityIdElement);
+                        XmlElement dataElement = xmlDocument.CreateElement("Data");
+                        logRecordElement.AppendChild(dataElement);
+                        XmlElement levelElement = xmlDocument.CreateElement("Level");
+                        levelElement.InnerText = Convert.ToString(traceEventType);
+                        dataElement.AppendChild(levelElement);
+                        XmlElement nameSpaceElement = xmlDocument.CreateElement("NameSpace");
+                        nameSpaceElement.InnerText = nameSpaceName;
+                        dataElement.AppendChild(nameSpaceElement);
+                        XmlElement classElement = xmlDocument.CreateElement("Class");
+                        classElement.InnerText = className;
+                        dataElement.AppendChild(classElement);
+                        XmlElement methodElement = xmlDocument.CreateElement("Method");
+                        methodElement.InnerText = methodName;
+                        dataElement.AppendChild(methodElement);
+                        XmlElement indexElement = xmlDocument.CreateElement("Index");
+                        indexElement.InnerText = Convert.ToString(index);
+                        dataElement.AppendChild(indexElement);
+                        XmlElement helpLinkElement = xmlDocument.CreateElement("HelpLink");
+                        helpLinkElement.InnerText = string.IsNullOrEmpty(exception.HelpLink) ? notAvailable : exception.HelpLink.Replace('\r', ' ').Replace('\n', ' ');
+                        dataElement.AppendChild(helpLinkElement);
+                        XmlElement messageElement = xmlDocument.CreateElement("Message");
+                        messageElement.InnerText = string.IsNullOrEmpty(exception.Message) ? notAvailable : exception.Message.Replace('\r', ' ').Replace('\n', ' ');
+                        dataElement.AppendChild(messageElement);
+                        XmlElement hResultElement = xmlDocument.CreateElement("HResult");
+                        hResultElement.InnerText = Convert.ToString(exception.HResult, 16).ToUpper();
+                        dataElement.AppendChild(hResultElement);
+                        XmlElement sourceElement = xmlDocument.CreateElement("Source");
+                        sourceElement.InnerText = string.IsNullOrEmpty(exception.Source) ? notAvailable : exception.Source.Replace('\r', ' ').Replace('\n', ' ');
+                        dataElement.AppendChild(sourceElement);
+
+                        XmlElement stackTraceElement = xmlDocument.CreateElement("StackTrace");
+                        stackTraceElement.InnerText = string.IsNullOrEmpty(exception.StackTrace) ? notAvailable : exception.StackTrace.Replace('\r', ' ').Replace('\n', ' ');
+                        dataElement.AppendChild(stackTraceElement);
+                        xmlDocument.Save(Path.Combine(logDirectory.FullName, logFileName));
+                    }
+                    catch (Exception)
+                    {
+                        return;
+                    }
+                    finally
+                    {
+                        logSemaphoreSlim?.Release();
                     }
                 }
             });
@@ -171,7 +179,7 @@ namespace PowerToolbox.Services.Root
                     string[] logFiles = Directory.GetFiles(logDirectory.FullName);
                     foreach (string logFile in logFiles)
                     {
-                        FileSystem.DeleteFile(logFile, UIOption.AllDialogs, RecycleOption.SendToRecycleBin);
+                        File.Delete(logFile);
                     }
 
                     return true;
@@ -189,17 +197,8 @@ namespace PowerToolbox.Services.Root
         /// </summary>
         public static void CloseLog()
         {
-            try
-            {
-                fileLogTraceListener.Dispose();
-                fileLogTraceListener = null;
-                logSemaphoreSlim.Dispose();
-                logSemaphoreSlim = null;
-            }
-            catch (Exception)
-            {
-                return;
-            }
+            logSemaphoreSlim.Dispose();
+            logSemaphoreSlim = null;
         }
     }
 }
